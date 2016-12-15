8b10986dc2d4ca176861e26de0c623c84069f692
hoffmr2 <hoffmr2@wp.pl>
2016-12-15 15:14:55 +0100

    implementacja ciągu fibonaciego

--- Father Sun/main.cpp
+++ Father Sun/main.cpp
@@ -6,6 +6,33 @@
 /*
  * Fmt - biblioteka do formatownia napsiów
  */
+
+
+int64_t FibonacciRek(int n)
+{
+	if (n == 0 || n == 1)
+		return n;
+	else
+		return FibonacciRek(n - 1) + FibonacciRek((n - 2));
+}
+
+
+int64_t FibonaciIter(int n)
+{
+	int64_t f0 = 0;
+	int64_t f1 = 1, f2=0;
+
+	for (int i=0;i<n;++i)
+	{
+		f2 = f0 + f1;
+		f0 = f1;
+		f1 = f2;
+	}
+	return f2;
+}
+
+
+
 int main()
 {
 
@@ -18,6 +45,7 @@ int main()
 	logger->info("Dzisiaj techday");
 	logger->debug("mówimy o c++");
 	logger->error("Coœ sie popsu³o");
+	logger->info("Fibonnaci Number: {}", FibonaciIter((3)));
 
 
 	return 0;
